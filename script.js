08e9651c8b09a13244cc5f52134dcf0cd519ca99
Sebastian Latkolic <latkolicsebastian@gmail.com>
2020-12-17 13:44:02 +0200

    upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
+// Disable/Enable Button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+
+// Passing Joke to VoiceRSS API
+function tellMe(joke) {
+    console.log("tell me ", joke);
+    VoiceRSS.speech({
+        key: "28f3196bb9fb405d8cd5293ce4ac4dff",
+        src: joke,
+        hl: "en-us",
+        v: "Linda",
+        r: 0,
+        c: "mp3",
+        f: "44khz_16bit_stereo",
+        ssml: false,
+    });
+}
+
+// Get Jokes from Joke API
+async function getJokes() {
+    let joke = "";
+    const apiUrl =
+        "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist";
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        // Text-to-Speech
+        tellMe(joke);
+        // Disable Button
+        toggleButton();
+    } catch (error) {
+        // Catch Errors here
+        console.log("whoops ", error);
+    }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
